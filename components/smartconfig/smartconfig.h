46dbe2497ce2844b820dd7d03f75143ba296cc0c
mhv2109 <marshall.vanloon@gmail.com>
2020-12-08 21:12:04 -0500

    add smartconfig component to connect to wifi

---
+++ components/smartconfig/smartconfig.h
@@ -0,0 +1,6 @@
+#ifndef SMARTCONFIG_H
+#define SMARTCONFIG_H
+
+void smartconfig__setup_wifi(void);
+
+#endif
--- main/main.c
+++ main/main.c
@@ -12,6 +12,7 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+#include "smartconfig.h"
 
 void app_main(void)
 {
@@ -30,11 +31,5 @@ void app_main(void)
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+   smartconfig__setup_wifi(); 
 }
